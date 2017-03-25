078083c5eae7e35a9e5600ab9fa0bd98f3b060cd
hemny-singh <mail.hemny@gmail.com>
2017-03-25 19:43:27 +0530

    Fixed autoplay and showing no. of views

--- app/js/controllers/dashboardController.js
+++ app/js/controllers/dashboardController.js
@@ -8,8 +8,9 @@ playlistMakerApp
   $scope.userPlayList = [];
   $scope.currentUser = {};
   $scope.currentVideo = {};
-  var videoInterval = 0;
   $scope.formError = false;
+  $scope.refreshRate = 0;
+  $scope.showCompletedMessage = false;
 
   function init() {
     var loggedinUser = playlistService.getData("currentUser");
@@ -44,33 +45,47 @@ playlistMakerApp
     }
     $scope.currentVideo.url = "https://www.youtube.com/embed/"+id+"?autoplay=1";
     $scope.currentVideo.title = playlistVideo.title;
+    $scope.currentVideo.views = playlistVideo.views;
   }
 
-  var asyncFunctionToPlayVideos = function (obj) {
-    var interval = obj.endTime - obj.startTime
-    var deferred = $q.defer()
-    setVideoFunction(obj)
-    $scope.timeoutPromise = $timeout(function() {
-      return deferred.resolve(true)
-    }, interval*1000);
-    return deferred.promise;
+  var calcCurrentVideo = function () {
+    var currentUser = angular.copy(playlistService.getData("users")[$scope.currentUser.email]);
+    $scope.userPlayList[$scope.curIndex].views = $scope.userPlayList[$scope.curIndex].views ? parseInt($scope.userPlayList[$scope.curIndex].views, 10) + 1 : 1;
+    currentUser.playlist = $scope.userPlayList;
+    playlistService.saveData('users', $scope.currentUser.email, currentUser)
+    setVideoFunction($scope.userPlayList[$scope.curIndex]);
+    $scope.curIndex++;
+    if ($scope.curIndex < $scope.userPlayList.length) {
+      $scope.refreshRate = $scope.userPlayList[$scope.curIndex].endTime - $scope.userPlayList[$scope.curIndex].startTime;
+    }
   }
+
+  $scope.$watch("refreshRate", function(oldVal, newVal) {
+    if (oldVal !== newVal && oldVal !== 0) {
+      $interval.cancel($scope.interValPromise);
+      $timeout.cancel($scope.timeoutPromise);
+      $scope.interValPromise = $interval(function() {
+        if ($scope.curIndex < $scope.userPlayList.length) {
+          calcCurrentVideo();
+        } else {
+          $scope.showCompletedMessage = true;
+          $interval.cancel($scope.interValPromise);
+        }
+    }, $scope.refreshRate*1000);
+    }
+  }, true);
 
   $scope.playYourPlaylist = function () {
     if (!$scope.userPlayList.length) {
       return;
     }
-    var i = 0;
-    $scope.interValPromise = $interval(function() {
-      asyncFunctionToPlayVideos($scope.userPlayList[i]).then(function(val) {
-        if (i < $scope.userPlayList.length) {
-          i++
-        } else {
-          console.log('play list ended')
-          $interval.cancel($scope.interValPromise);
-        }
-      })
-    }, 10)
+    $scope.showCompletedMessage = false;
+    $scope.curIndex = 0;
+    $scope.refreshRate = 0;
+
+    $scope.timeoutPromise = $timeout(function() {
+      calcCurrentVideo()
+    }, $scope.refreshRate);
   }
 
   $scope.$on('$destroy',function() {
@@ -99,6 +114,7 @@ playlistMakerApp
       if (loggedinUser && loggedinUser.hasOwnProperty("email")) {
         var currentUser = angular.copy(playlistService.getData("users")[loggedinUser.email]),
           userPlayList = angular.copy(currentUser.playlist) || []
+        $scope.playlistObj.views = 0;
         userPlayList.push($scope.playlistObj)
         currentUser.playlist = val ? userPlayList : []
         playlistService.saveData('users', loggedinUser.email, currentUser)
